121bf870280c7774ea0ae0f74910ac5ff78c1671
Gedyael Lucas <lucasgedyael@gmail.com>
2023-06-08 21:05:29 -0300

    modificaçao das imagens e  adicionei uma vairavel

--- main.py
+++ main.py
@@ -24,14 +24,34 @@ def Nova_janela():
     master1.title('Pybank')
     master1.geometry('323x700+610+153')
 
+        
+    #Colar as linhas no código principal. OBS: a instância da classe Tk() deve ser de mesmo nome!
+    master1.bind('<Button-1>', posiciona.inicio_place)
+    master1.bind('<ButtonRelease-1>', lambda arg: posiciona.fim_place(arg, master1))
+    master1.bind('<Button-2>', lambda arg: posiciona.para_geometry(master1))
     #bordas de janela
     master1.wm_resizable(width=0,height=0)
     #impotaçao nova janela
     #imagem Secudaria
-    imagem_secundaria = PhotoImage(file="imagens/Tela_sec.png")
+    imagem_secundaria = PhotoImage(file="imagens/Tela_2.png")
     #criaçao da Label
     tela_secundaria = Label(master1,image=imagem_secundaria)
     tela_secundaria.pack()
+    saldo = StringVar()
+    saldo.set('0,90')
+    
+    # CRIAÇAO DE CAIXA DE SAIDA
+    # test = Label(master1,text='pix')
+    # test.place(width=76, height=30, x=2, y=144)
+    #CRIAÇAO DO SEGUNDO SALDO
+    test1 = Label(master1,textvariable=saldo,font='verdana  14 bold')
+    test1.place(width=54, height=26, x=44, y=159)
+    #botao_pix = Button(master1,)
+    
+    master1.mainloop()
+
+
+
 
 # Importações de imagens:
 # Imagem pricipal:
