21beb2b26e2cc27fb5f5906518579447b1fa9c7f
Gedyael Lucas <lucasgedyael@gmail.com>
2023-06-13 23:39:27 -0300

    modificaçoes no projeto inserindo uma imagem nova

--- main.py
+++ main.py
@@ -38,18 +38,34 @@ def Nova_janela():
     tela_secundaria = Label(master1,image=imagem_secundaria)
     tela_secundaria.pack()
     saldo = StringVar()
-    saldo.set('0,90')
-    
+    saldo.set('100,90')
+    pix = StringVar()
+    pix.set('PIX')
     # CRIAÇAO DE CAIXA DE SAIDA
     # test = Label(master1,text='pix')
     # test.place(width=76, height=30, x=2, y=144)
     #CRIAÇAO DO SEGUNDO SALDO
-    test1 = Label(master1,textvariable=saldo,font='verdana  14 bold')
-    test1.place(width=54, height=26, x=44, y=159)
-    #botao_pix = Button(master1,)
-    
+    test1 = Label(master1,textvariable=saldo,font='verdana  12 bold')
+    test1.place(width=90, height=16, x=43, y=164)
+
+    #botao2 = Label(master1,textvariable=pix,font='verdana')
+    botao_pix = Button(master1,textvariable=pix,command=Janela_2,font='verdana 7 bold',borderwidth=0)
+    botao_pix.place(width=50, height=20, x=20, y=278)
+
     master1.mainloop()
 
+def Janela_2():
+    time.sleep(0.3)
+    master2 = Tk()
+    master2.title('Area Pix')
+    master2.geometry('323x700+610+153')
+    master2.wm_resizable(width=0,height=0)
+
+    imagem_ter = PhotoImage(file="imagens/Tela_3.png")
+    Tela_ter = Label(master2,image=imagem_ter)
+    Tela_ter.pack()
+    master2.mainloop()
+    
 
 
 
